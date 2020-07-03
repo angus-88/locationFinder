2524ca6d36e3c2d654b11ea4c9b0bcd75d9c24f4
Angus <angus.h88@outlook.com>
2020-07-03 15:59:15 +0100

    added browser instruction

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       </header>
       <main>
         <h2>Get addresses based on Long and Lat</h2>
+            <p>Please use Google Chrome, other browsers may not work as expected</p>
         <p>Each CSV file must at least have the Latitude and Longtitude headers in the first row</p>
         <p>Files are processed in the broswer locally and not sent anywhere</p>
         <p>Refreshing the page will cause you to lose any files in progress</p>
